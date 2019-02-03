278edf55287afd954bc553003c898f5072e4a5c4
Dylan <40635921+robothomas@users.noreply.github.com>
2019-02-02 23:23:07 -0600

    Change format

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,13 +7,6 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -22,21 +15,59 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  */
 public class RobotMap {
   
+  // Controllers
+  public static final int XBOX_CONTROLLER_PORT = 0;
+
+  // Axes
+
+  public static final int LEFT_STICK_Y = 1;
+
+  public static final int RIGHT_STICK_Y = 5;
+
+  // Buttons (X Mode)
+  
+  public static final int X_BUTTON = 1;
+
+  public static final int Y_BUTTON = 2;
+
+  public static final int A_BUTTON = 3;
+
+  public static final int B_BUTTON = 4;
+
+  public static final int LEFT_BUMPER = 5;
+
+  public static final int RIGHT_BUMPER = 6;
+
+  public static final int LEFT_TRIGGER = 7;
+
+  public static final int RIGHT_TRIGGER = 8;
+
+  public static final int BACK_BUTTON = 9;
+
+  public static final int START_BUTTON = 10;
+
+  public static final int LEFT_JOYSTICK_TRIGGER = 11;
+
+  public static final int RIGHT_JOYSTICK_TRIGGER = 12;
+
   // Drive
-  public static final WPI_TalonSRX RRearWheel = new WPI_TalonSRX(1); // right rear wheel
-  public static final WPI_TalonSRX RFrontWheel = new WPI_TalonSRX(0); // right front wheel
-  public static final WPI_TalonSRX LRearWheel = new WPI_TalonSRX(3); // left rear wheel
-  public static final WPI_TalonSRX LFrontWheel = new WPI_TalonSRX(2); // left front wheel
+  public static final int RIGHT_REAR_WHEEL_PORT = 1;
 
-  public static final SpeedControllerGroup right = new SpeedControllerGroup(RRearWheel, RFrontWheel); // right speed controller group 
-  public static final SpeedControllerGroup left = new SpeedControllerGroup(LRearWheel, LFrontWheel); // left speed controller group 
+  public static final int RIGHT_FRONT_WHEEL_PORT = 0;
 
-  public static final DifferentialDrive roboDrive = new DifferentialDrive(left, right); // making both speed controllers part of the overall drive
+  public static final int LEFT_REAR_WHEEL_PORT = 3;
+
+  public static final int LEFT_FRONT_WHEEL_PORT = 2;
 
   // Phenumatics
-  public static final Compressor BigChungus = new Compressor(0);
-  public static final Solenoid NeckPusher = new Solenoid(0, 0);
-  public static final Solenoid BeakPusher = new Solenoid(0, 4);
-  public static final Solenoid TailMover = new Solenoid(0, 2);
+  public static final int COMPRESSOR_MODULE = 0;
+
+  public static final int NECK_SOLENOID_CHANNEL = 0;
+
+  public static final int BEAK_SOLENOID_CHANNEL = 4;
+
+  public static final int TAIL_SOLENOID_FOWARD_CHANNEL = 1;
+
+  public static final int TAIL_SOLENOID_REVERSE_CHANNEL = 2;
 
 }
