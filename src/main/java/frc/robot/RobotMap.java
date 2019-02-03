56e98e4745c9c8593c8095038de31b22c2e07b4c
Dylan <40635921+robothomas@users.noreply.github.com>
2019-02-03 11:28:27 -0600

    Change port numbers

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -25,14 +25,14 @@ public class RobotMap {
   public static final int RIGHT_STICK_Y = 5;
 
   // Buttons (X Mode)
-  
-  public static final int X_BUTTON = 1;
 
-  public static final int Y_BUTTON = 2;
+  public static final int A_BUTTON = 1;
 
-  public static final int A_BUTTON = 3;
+  public static final int B_BUTTON = 2;
 
-  public static final int B_BUTTON = 4;
+  public static final int X_BUTTON = 3;
+
+  public static final int Y_BUTTON = 4;
 
   public static final int LEFT_BUMPER = 5;
 
