b2ae2d3d38d4665d4a6dc0c5671c22caa62a2c8e
Dylan <40635921+robothomas@users.noreply.github.com>
2019-02-09 12:16:47 -0600

    Add RobotState for changing variables

---
+++ src/main/java/frc/robot/RobotState.java
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * Keeps track of changing variables that track the state of the robot, such as whether the beak is
+ * open or closed, if the neck is extended or retracted, or if the ramp has been deployed yet.
+ */
+public class RobotState {
+
+    // State Variables
+
+    public static boolean beakOut = false;
+
+    public static boolean tailOut = false;
+
+    public static boolean neckOut = false; 
+
+    public static boolean notDeployed = true;
+}
