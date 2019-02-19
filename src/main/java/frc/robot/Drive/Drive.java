1e9e348e48c7e911897db557c1a2f63d6ef4ade2
Admin <45888220+Robojack-Admin@users.noreply.github.com>
2019-02-19 17:47:56 -0600

    Add sensitivity settings

--- src/main/java/frc/robot/Drive/Drive.java
+++ src/main/java/frc/robot/Drive/Drive.java
@@ -9,7 +9,10 @@ package frc.robot.Drive;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Constants;
 import frc.robot.Robot;
+import frc.robot.RobotMap;
+import frc.robot.RobotState;
 
 /**
  * The Drive system, utilizing xbox controllers and tank drive to control the robot.
@@ -22,17 +25,26 @@ public class Drive extends Command {
 
   public Drive() {
     requires(Robot.drivetrain);
+    requires(Robot.limelight);
   }
 
   // Called once when the command executes
   @Override
   protected void initialize() {
     Robot.drivetrain.stop();
+    Robot.limelight.lightOff();
+    Robot.limelight.driverMode();
   }
 
   @Override
   protected void execute() {
-    Robot.drivetrain.TankDrive(-xbox.getRawAxis(1), -xbox.getRawAxis(5));
+    
+    if (RobotState.sensitive) {
+      Robot.drivetrain.TankDrive(Constants.lowGear*xbox.getRawAxis(5), Constants.lowGear*xbox.getRawAxis(1));
+    } else {
+      Robot.drivetrain.TankDrive(Constants.highGear*xbox.getRawAxis(5), Constants.highGear*xbox.getRawAxis(1));
+    }
+    
   }
 
   @Override
