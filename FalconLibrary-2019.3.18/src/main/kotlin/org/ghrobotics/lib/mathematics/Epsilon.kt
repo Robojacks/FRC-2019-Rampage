4f9824381fcdfe765c45c7ca8df4c38b3ade9796
Dylan <robothomas@users.noreply.github.com>
2019-03-24 16:37:30 -0500

    Delete library source code

--- FalconLibrary-2019.3.18/src/main/kotlin/org/ghrobotics/lib/mathematics/Epsilon.kt
+++
@@ -1,8 +0,0 @@
-/*
- * FRC Team 5190
- * Green Hope Falcons
- */
-
-package org.ghrobotics.lib.mathematics
-
-const val kEpsilon = 1E-9
--- FalconLibrary-2019.3.18/src/main/kotlin/org/ghrobotics/lib/types/VaryInterpolatable.kt
+++
@@ -1,5 +0,0 @@
-package org.ghrobotics.lib.types
-
-interface VaryInterpolatable<S> : Interpolatable<S> {
-    fun distance(other: S): Double
-}
