ec4110011c277bc2bd0f8c66a244b9a963ed4f24
Robojack Admin <first4674@isd31.net>
2019-06-25 09:24:56 -0500

    Add path follower helper class

---
+++ src/main/java/frc/robot/Auto/PathFollower.java
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.Auto;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.I2C.Port;
+
+/**
+ * Add your docs here.
+ */
+public class PathFollower {
+    AHRS navX = new AHRS(Port.kMXP);
+
+    
+
+
+
+}
